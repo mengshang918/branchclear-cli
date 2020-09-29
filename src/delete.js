0fc899d9d6dbc0c58c6fa278906a6158fec9b977
mengshang918 <1446269124@qq.com>
2020-09-29 17:27:00 +0800

    chore: 删除本地分支时，排除当前分支和主分支

--- src/delete.js
+++ src/delete.js
@@ -3,7 +3,7 @@
  * @Author: jiangxiaowei
  * @Date: 2020-09-29 16:39:30
  * @Last Modified by: jiangxiaowei
- * @Last Modified time: 2020-09-29 17:09:41
+ * @Last Modified time: 2020-09-29 17:23:07
  */
 /* {
   main: 'master',
@@ -20,12 +20,16 @@ const execa = require('execa')
 const { log } = console
 
 module.exports = (branchLocal, branchRemote, options) => {
-  const { remoteName } = options
+  const { remoteName, main } = options
   // TODO: 先校验当前分支是否存在，不存在跳过。并提示不存在的分支
-  branchLocal.map(async (item) => {
-    const res = await execa('git', ['branch', '-D', item])
-    log(res)
-  })
+  const reg = new RegExp(`^\\*|${main}`)
+  console.log(reg)
+  branchLocal
+    .filter((item) => !reg.test(item))
+    .map(async (item) => {
+      const res = await execa('git', ['branch', '-D', item])
+      log(res)
+    })
   branchRemote.map(async (item) => {
     const res = await execa('git', ['push', remoteName, '--delete', item])
     log(res)
